173bca54f8f39696e974afe657ea3dcc56a51309
Kanis_Fatema_Shanta <33979816+Kanis-Fatema-Shanta@users.noreply.github.com>
2020-08-28 23:08:14 +0600

    complete online course

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className="footer">
-            &copy; Kanis Fatema Shanta| 2020
+            &copy; Kanis Fatema Shanta | 2020
         </div>
     );
 };
